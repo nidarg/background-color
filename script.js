90e3818ee3087a115b9c334cf426a32383307190
nidarg <gradin_g@yahoo.com>
2020-06-23 00:16:44 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,16 @@
+var gradient = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("bodyGradient");
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right, "
+                            +color1.value
+                            +", "
+                            +color2.value
+                            +")";
+    gradient.textContent = body.style.background+ ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
